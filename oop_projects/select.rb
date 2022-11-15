ff57c7c4abf6cfeddef7f8f3af37e40ea69610e8
ganstacoder <ganstacoder@gmail.com>
2022-11-14 19:44:03 -0800

    More Ruby

--- oop_projects/select.rb
+++ oop_projects/select.rb
@@ -11,6 +11,23 @@ movie_ratings = {
   lion_king: 3.5
 }
 
-good_movies = movie_ratings.select { |name, rating| rating > 3 }
+good_movies = movie_ratings.select { |name, rating| rating > 3 } # Select movies with rating greater than 3
 
 puts good_movies
+
+
+movie_ratings1 = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 3,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+
+movie_ratings1.each_key { |title| puts title } # Iterate through each key and print key
+
+puts movie_ratings1
